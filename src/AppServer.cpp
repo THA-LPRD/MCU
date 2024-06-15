53298b7d38d9e0e25fd0704de17d0a1de0512038
mwegmann <mario.wegmann@hs-augsburg.de>
2024-06-15 14:41:09 +0200

    Implemented registering new display, fetching imagepath from database.

--- src/AppServer.cpp
+++ src/AppServer.cpp
@@ -21,18 +21,87 @@ bool AppServer::Init() {
 
     // TODO: Server URL in Config
 
-    // Fetch newest Config from Server
+    // Register as new display on server
+    Log::Debug("Registering as new display on server");
+
+    // Print ESP32 MAC address
+    uint8_t mac[6];
+    esp_efuse_mac_get_default(mac);
+    Log::Info("ESP32 MAC address: %02X:%02X:%02X:%02X:%02X:%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+    // Convert MAC address to string
+    char registerURL[65];
+    sprintf(registerURL, "http://192.168.10.150:3000/api/v1/displays/register/%02X%02X%02X%02X%02X%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+
+    // Create payload from JSON
+    DynamicJsonDocument newDisplayPayload(1024);
+    newDisplayPayload["friendly_name"] = "Display1";
+    newDisplayPayload["width"] = EPDL::GetWidth();
+    newDisplayPayload["height"] = EPDL::GetHeight();
+
+    // Serialize JSON to string
+    String newDisplayPayloadString;
+    serializeJson(newDisplayPayload, newDisplayPayloadString);
+
+    http.begin(registerURL);
+
+    http.addHeader("Content-Type", "application/json");
+    http.addHeader("Content-Length", String(newDisplayPayloadString.length()));
+    // http.setPayload(payloadString);
+ 
+    int httpCode = http.PUT(newDisplayPayloadString);
+
+    if (httpCode == HTTP_CODE_OK) {
+        Log::Info("Registered as new display on server");
+    }
+    else {
+        Log::Error("Failed to register as new display, HTTP code: %d", httpCode);
+    }
+
+    http.end();
+
+    // Get newest config from server
     Log::Debug("Fetching config from server");
 
-    http.begin("http://192.168.10.150:3000/");
+    String imageURLPath;
+    char configURL[65];
+    sprintf(configURL, "http://192.168.10.150:3000/api/v1/displays/config/%02X%02X%02X%02X%02X%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+
+    http.begin(configURL);
+
+    int httpCode = http.GET();
+
+    if (httpCode == HTTP_CODE_OK) {
+        Log::Info("Config fetched successfully");
+        String configPayloadString = http.getString();
+        Log::Debug("Config: %s", configPayloadString.c_str());
+        // Parse JSON
+        DynamicJsonDocument configPayload(1024);
+        DeserializationError error = deserializeJson(configPayload, configPayloadString);
+        if (error) {
+            Log::Error("Failed to parse JSON config");
+            return false;
+        } else {
+            // Set config values
+            imageURLPath = configPayload["file_path"].as<String>();
+            // Config::Set(Config::Key::OperatingMode, configPayload["operating_mode"].as<String>());
+            
+        }
+        // Set config values
+    }
+    else {
+        Log::Error("Failed to fetch config, HTTP code: %d", httpCode);
+    }
 
     // Fetch newest Image from Server
     Log::Debug("Fetching image from server");
 
     std::ofstream file;
     std::string path = "/upload/";
+
+    char imageURL[100];
+    sprintf(imageURL, "http://192.168.10.150:3000%S", imageURLPath.c_str());
     
-    http.begin("http://192.168.10.150:3000/uploads/image.png"); // Replace with the actual URL of the image
+    http.begin(imageURL); // Replace with the actual URL of the image
     int httpCode = http.GET();
     if (httpCode == HTTP_CODE_OK) {
         Log::Debug("Location: %s", http.getLocation().c_str());
@@ -42,7 +111,6 @@ bool AppServer::Init() {
 
         WiFiClient* stream = http.getStreamPtr();
 
-        // File file = SPIFFS.open("/image.png", FILE_WRITE); // Replace with the desired file path
         if (!file) {
             Log::Error("Failed to open file for writing");
             return false;
@@ -69,12 +137,6 @@ bool AppServer::Init() {
     EPDL::DrawImage(m_ImageHandle, 0, 0);
     EPDL::SwapBuffers();
     EPDL::EndFrame();
-
-    // Get newest Image from Server
-
-
-
-    // Show Image
 
     // Sleep
 
