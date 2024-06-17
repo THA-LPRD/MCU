af5a0689023ad996321e34a943a5b16f8bd5ae82
mwegmann <mario.wegmann@hs-augsburg.de>
2024-06-17 08:48:59 +0200

    New TimeTable2 Template; Implemented DeepSleep in AppServer mode

--- src/AppServer.cpp
+++ src/AppServer.cpp
@@ -9,6 +9,9 @@
 #include "Filesystem.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "esp_sleep.h"
+#include "nvs_flash.h"
+#include "driver/rtc_io.h"
 
 #include <fstream>
 
@@ -31,25 +34,49 @@ bool AppServer::Init() {
     Log::Debug("Checking if display is already registered on server");
 
     uint8_t mac[6];
+    int secondsToSleep = 300;
     esp_efuse_mac_get_default(mac);
 
     if (CheckRegistered(mac)) {
         Log::Info("Display is already registered on server");
-        String imageURLPath = GetNewConfig(mac);
-        DisplayImage(GetNewImage(imageURLPath));
+        String configPayloadString = GetNewConfig(mac);
+        // Parse JSON
+        DynamicJsonDocument configPayload(1024);
+        DeserializationError error = deserializeJson(configPayload, configPayloadString);
+        if (error) {
+            Log::Error("Failed to parse JSON config");
+        } else {
+            // Set config values
+            secondsToSleep = configPayload["valid_for"].as<int>();
+            String imageURLPath = configPayload["file_path"].as<String>();
+            DisplayImage(GetNewImage(imageURLPath));            
+        }
+        // Set config values
+        
     } else {
         Log::Info("Display is not yet registered on server");
         RegisterDisplay(mac);
     }
 
-    // Sleep
-
+    // Finished showing image, disconnecting WiFi and going to sleep
     DisconnectWiFi();
+
+    Log::Debug("Enabling timer wakeup in %d s", secondsToSleep);
+    esp_sleep_enable_timer_wakeup(secondsToSleep * 1000000);
+
+    // TODO: Change to <= 21 for ESP32-S3
+    gpio_num_t ext_wakeup_pin_0 = GPIO_NUM_1;
+    Log::Debug("Enabling GPIO wakeup on Pin %d", ext_wakeup_pin_0);
+
+    esp_sleep_enable_ext0_wakeup(ext_wakeup_pin_0, 1);
+    rtc_gpio_pullup_dis(ext_wakeup_pin_0);
+    rtc_gpio_pulldown_en(ext_wakeup_pin_0);
+    
     return true;
 }
 
 void AppServer::Run() {
-    // Not used in Servermode. 
+    esp_deep_sleep_start();
 }
 
 bool AppServer::SetupWiFi() {
@@ -146,7 +173,7 @@ String AppServer::GetNewConfig(uint8_t *mac) {
 // Get newest config from server
     Log::Debug("Fetching config from server");
     HTTPClient http;
-    String imageURLPath;
+    String configPayloadString = "";
     char configURL[65];
     sprintf(configURL, "%S/api/v1/displays/config/%02X%02X%02X%02X%02X%02X", Config::Get(Config::Key::ServerURL).c_str(), mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
 
@@ -156,28 +183,14 @@ String AppServer::GetNewConfig(uint8_t *mac) {
 
     if (httpCode == HTTP_CODE_OK) {
         Log::Info("Config fetched successfully");
-        String configPayloadString = http.getString();
+        configPayloadString = http.getString();
         Log::Debug("Config: %s", configPayloadString.c_str());
-        // Parse JSON
-        DynamicJsonDocument configPayload(1024);
-        DeserializationError error = deserializeJson(configPayload, configPayloadString);
-        if (error) {
-            Log::Error("Failed to parse JSON config");
-        } else {
-            // Set config values
-            imageURLPath = configPayload["file_path"].as<String>();
-            // Config::Set(Config::Key::OperatingMode, configPayload["operating_mode"].as<String>());
-            
-        }
-        // Set config values
-
     }
     else {
         Log::Error("Failed to fetch config, HTTP code: %d", httpCode);
-        
     }
     http.end();
-    return imageURLPath;
+    return configPayloadString;
 }
 
 std::string AppServer::GetNewImage(String imageURLPath) {
